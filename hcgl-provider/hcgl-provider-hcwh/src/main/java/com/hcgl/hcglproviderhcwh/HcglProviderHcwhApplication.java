1535e61bfef430a069db09ec4b3ef11f29947041
xuzhuosheng <284696920@qq.com>
2019-10-10 11:52:35 +0800

    解决超时问题

--- hcgl-provider/hcgl-provider-hcwh/src/main/java/com/hcgl/hcglproviderhcwh/HcglProviderHcwhApplication.java
+++ hcgl-provider/hcgl-provider-hcwh/src/main/java/com/hcgl/hcglproviderhcwh/HcglProviderHcwhApplication.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan ("com.hcgl.hcglproviderhcwh.dao")
+@EnableFeignClients
 public class HcglProviderHcwhApplication {
     public static void main(String[] args) {
         SpringApplication.run(HcglProviderHcwhApplication.class, args);
